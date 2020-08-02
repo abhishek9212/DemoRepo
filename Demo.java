fc381ea6ac029344765f57c123b6011bb1130bea
abhishek9212 <abhishekyadav4538@gmail.com>
2020-08-02 12:38:28 +0530

    Revert "added demo file"
    This reverts commit 04578d1d2f5015f2af5fcd8eaa4997ede5fd43b8.
    
    Conflicts:
    	Demo.java

--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
 public class Demo{
 	public static void main(String[] args){
 		System.out.println("This is a demo program");
-		System.out.println("Git is very easy to operate");
-		System.out.println("Abhishek changed and another modification");
+
+	
  	}
 } 
