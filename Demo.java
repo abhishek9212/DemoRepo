a8dc4a34f6ef426c18ec20801ec9dfd189657633
abhishek9212 <abhishekyadav4538@gmail.com>
2020-08-05 22:35:49 +0530

    modification done

--- Demo.java
+++ Demo.java
@@ -1,8 +1,7 @@
 public class Demo{
 	public static void main(String[] args){
 		System.out.println("This is a demo program");
-		System.out.println("modification done");
+		System.out.println("Modification done");
 
-	
- 	}
 } 
+}
